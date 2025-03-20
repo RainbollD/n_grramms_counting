ed11dc5d085eccb14300cd5c8a3ab265935d3dbb
Lev <rogulev0805@gmail.com>
2025-03-20 17:18:04 +0300

    fix: n:[1-20]

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 STOP_WORDS = ["так", "то", "вот", "уже", "ни", "же", "не", "бы", "а", "там", "чтоб", "ли", "ли", "б", "хоть", "уж"],
 FOLDER_SAVE = 'result_ngramms'
-N_GRAMMS = tuple((i for i in range(2, 21)))
+N_GRAMMS = tuple((i for i in range(1, 21)))
